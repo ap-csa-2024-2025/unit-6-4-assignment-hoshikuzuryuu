68eb22af722788e0436b446de3a08dc065661ea0
hoshikuzuryuu <yaminoiroha@gmail.com>
2025-02-12 10:22:04 -0800

    agafhgsgdg

--- Main.java
+++ Main.java
@@ -4,31 +4,75 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create some arrays here to test your methods
-    
+    String[] stringArr =
+    {
+      "sword", "shield", "axe", "staff", "sword", "sword", "sword", "axe", "axe", "axe", "shield", "staff"
+    };
+    String mode = findMode(stringArr);
   }
 
   public static int countLength(String[] arr, int targetLength)
   {
-    // replace with your code
+    int count = 0; 
+    
+    for (String str: arr) 
+    {
+      if (str.length() == targetLength)
+      {
+        count++;
+      }
+      return count;
+    }
     return 0;
   }
 
   public static int indexOf(double[] arr, double target)
   {
-    // replace with your code
+    for (int i = 0; i < arr.length; i++)
+    {
+      if (arr[i] == target)
+      {
+        return i; 
+      }
+    }
     return -1;
   }
 
   public static boolean hasDuplicates(int[] arr)
   {
-    // replace with your code
+    for (int i = 0; i < arr.length; i++)
+    {
+      for (int j = i+1; j < arr.length; j++)
+      {
+        if (arr[i] == arr[j])
+        {
+          return true;
+        }
+      }
+    }
     return false;
   }
 
-  public static String findMode(String[] arr)
+  public static String findMode(String[] arr) 
   {
-    // replace with your code
-    return null;
+    String mode = null; 
+    int maxCount = 0; 
+    for (int i = 0; i < arr.length; i++)
+    {
+      int count = 0; 
+      for (int j = i+1; j < arr.length; j++)
+      {
+        if (arr[i].equals(arr[j]))
+        {
+          count++;  
+        }
+      }
+      if (count > maxCount)
+      {
+        maxCount = count; 
+        mode = arr[i];
+      }
+    }
+    return mode;
   }
 }
